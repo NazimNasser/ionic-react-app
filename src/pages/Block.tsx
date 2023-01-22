1c92a208f794f5232201c3fc5093666c459a8dda
M.Khalid.K.Agha <mkhalid.k.agha@gmail.com>
2023-01-22 18:02:48 +0200

    Block Page Updated

--- src/pages/Block.tsx
+++ src/pages/Block.tsx
@@ -1,21 +1,27 @@
-import { IonContent, IonHeader, IonLabel, IonSelect, IonSelectOption, IonImg, IonPage, IonTitle, IonButton } from '@ionic/react'
+import { IonContent, IonLabel, IonSelect, IonSelectOption, IonImg, IonPage, IonButton, IonFooter, IonTitle, IonToolbar, IonNav, IonHeader } from '@ionic/react'
 import React, { useState } from 'react'
 
 const Block: React.FC = (props) => {
 
-    const [buttonStatus, setButtonStatus] = useState(false)
-    // Disable button
-    const disableButton = () => {
-        if (document.querySelector('.IonSelectOption')?.nodeValue === '') {
-            console.log('gggg',buttonStatus);
-            
-        } else {
-            console.log('bbbbbbbeeee',buttonStatus);
-            
-            // setButtonStatus(!buttonStatus)
+    // Disable button  
+    const initialValues = { desk: '', office: '', officeNumber: '' };
+    const [options, setOptions] = useState(initialValues);
+
+
+    const handleChangeModal = (event: any) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        setOptions({ ...options, [name]: value });
+    }
+
+    const checkIfEmpty = (obj: any) => {
+        let status = false
+        for (let key in obj) {
+            if (obj[key] === "") {
+                status = true
+            }
         }
-
-
+        return status;
     }
 
     return (
@@ -24,12 +30,11 @@ const Block: React.FC = (props) => {
 
             <IonContent fullscreen >
 
-                <nav className='bg-[#375A64] w-full relative flex flex-col items-center py-8 justify-around h-1/3'>
-                    <h1 className='text-[32px] font-[400] text-[#FFC800]'>
-                        Comfortline
-                    </h1>
-                    <p className='text-white'>Welcome to BLOCK C</p>
-                </nav>
+                {/* Header */}
+                <IonHeader className='bg-[#375A64] relative w-full -z-50  h-1/3 flex flex-col items-center py-8 mb-4 justify-around'>
+                    <IonTitle className=' font-semibold text-4xl leading-10 text-[#FFC800]'>Comfortline</IonTitle>
+                    <p className='text-white font-normal text-lg leading-6'>Welcome to BLOCK C</p>
+                </IonHeader>
 
                 <div className='w-full h-full relative '>
                     <div className='w-[90%] h-auto shadow-formShadow bg-white m-auto left-0.5 right-0.5 rounded-[20px] absolute -top-8 flex flex-col items-center px-8 py-4'>
@@ -47,54 +52,66 @@ const Block: React.FC = (props) => {
                         </article>
 
                         {/* form */}
-                        <form className=' 400 w-full'>
-                            {/* Options */}
+                        <form className='w-full'>
+                            {/* Option */}
                             <div className='w-full bg-[#EDF4F5] my-4 px-1 rounded-2xl flex flex-row justify-around items-center'>
                                 <IonLabel className='py-4 '>Do you have a fixed desk?</IonLabel>
-                                <IonSelect >
-                                    <IonSelectOption value="nes">yes</IonSelectOption>
-                                    <IonSelectOption value="n64">NO</IonSelectOption>
+                                <IonSelect
+                                    name="desk"
+                                    onIonChange={handleChangeModal}
+                                >
+                                    <IonSelectOption value="yes">yes</IonSelectOption>
+                                    <IonSelectOption value="no">NO</IonSelectOption>
 
                                 </IonSelect>
                             </div>
-                            {/* Options */}
+
+                            {/* Option */}
                             <div className='w-full bg-[#EDF4F5] my-4 px-1 rounded-2xl flex flex-row justify-around items-center'>
                                 <IonLabel className='py-4 '>Is it an individual office or <br /> an open space?</IonLabel>
-                                <IonSelect >
-                                    <IonSelectOption value="nes">yes</IonSelectOption>
-                                    <IonSelectOption value="n64">NO</IonSelectOption>
-
+                                <IonSelect
+                                    name="office"
+                                    onIonChange={handleChangeModal}
+                                >
+                                    <IonSelectOption value="yes">yes</IonSelectOption>
+                                    <IonSelectOption value="no">NO</IonSelectOption>
                                 </IonSelect>
                             </div>
 
-                            {/* Options */}
+                            {/* Option */}
                             <div className='w-full bg-[#EDF4F5] my-4 px-1 rounded-2xl flex flex-row justify-around items-center'>
                                 <IonLabel className='py-4 '>Please indicate the office <br /> number you are working in</IonLabel>
-                                <IonSelect >
+                                <IonSelect
+                                    name="officeNumber"
+                                    onIonChange={handleChangeModal}
+                                >
                                     <IonSelectOption value="1">1</IonSelectOption>
                                     <IonSelectOption value="2">2</IonSelectOption>
-
                                 </IonSelect>
                             </div>
 
                             {/* button */}
-                            <IonButton expand='block' className=' rounded bg-[#375A64] disabled:bg-[#E3E3E3]'
-                                // disabled
-                                onClick={()=>disableButton()}
+                            <IonButton
+                                expand='block'
+                                className='rounded bg-[#375A64] '
                                 color={'#375a64'}
-                            >
-                                CONTINUE
+                                disabled={checkIfEmpty(options)}
+                            >CONTINUE
                             </IonButton>
                         </form>
                     </div>
-
-                    {/* Rights */}
                 </div>
 
-                {/* Footer */}
-                <footer className='w-full text-center'>
-                    &copy;ALL RIGHT RESERVED
-                </footer>
+
+                <IonFooter
+                    translucent className='ion-no-border border-t-0  '
+                >
+                    <IonToolbar className='text-center  ' >
+                        <IonTitle className='text-[#828282] font-[Inter] font-normal text-[9px]'>
+                            &copy;ALL RIGHT RESERVED
+                        </IonTitle>
+                    </IonToolbar>
+                </IonFooter>
 
             </IonContent >
         </IonPage >
@@ -104,3 +121,4 @@ const Block: React.FC = (props) => {
 }
 
 export default Block;
+ 
